82b27c41f94bb8686b445130b770e94f0100c7ff
Rahmat <rahmat14121996@gmail.com>
2023-10-31 03:04:38 +0000

    upgrade major module go-say-hello

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/rahmat1996/app-say-hello
 go 1.21.2
 
 require github.com/rahmat1996/go-say-hello v1.1.0
+
+require github.com/rahmat1996/go-say-hello/v2 v2.0.0
--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	go_say_hello "github.com/rahmat1996/go-say-hello"
+	go_say_hello2 "github.com/rahmat1996/go-say-hello/v2"
 )
 
 func main() {
 	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello2.SayHello("Rahmat"))
 }
