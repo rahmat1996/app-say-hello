cb719ab7fb1f4f31917698f8f1d9f0d9df25cd94
Rahmat <rahmat14121996@gmail.com>
2023-10-31 02:29:37 +0000

    initial commit app-say-hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rahmat1996/app-say-hello
+
+go 1.21.2
+
+require github.com/rahmat1996/go-say-hello v1.1.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/rahmat1996/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
